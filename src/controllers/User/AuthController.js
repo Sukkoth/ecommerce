f7a681486042eb673067b2e9860746b7e1ec80bb
Sukkoth <suukootj@gmail.com>
2023-08-08 09:06:53 +0300

    added esLint configuration

--- src/controllers/User/AuthController.js
+++ src/controllers/User/AuthController.js
@@ -1,31 +1,29 @@
 const User = require('../../models/User');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const asyncHanlder = require('express-async-handler');
 const createUserValidation = require('../../validation/User/createUser');
 const parseValidationErrors = require('../../utils/parseValidationErrors');
 const loginValidation = require('../../validation/User/loginUser');
 const generateToken = require('../../utils/generateToken');
-
 /**
  * @desc register a user
  * @route POST /users/auth/register
  * @returns {object} user
  */
 const register = asyncHanlder(async (req, res) => {
-    const { error: validationError, value: validated } =
-        createUserValidation.validate(req.body, { abortEarly: false });
+  const { error: validationError, value: validated } =
+    createUserValidation.validate(req.body, { abortEarly: false });
 
-    if (validationError)
-        return res.json(parseValidationErrors(validationError.details));
+  if (validationError)
+    return res.json(parseValidationErrors(validationError.details));
 
-    const hashedPassword = await bcrypt.hash(validated.password, 10);
+  const hashedPassword = await bcrypt.hash(validated.password, 10);
 
-    const user = await User.create({ ...validated, password: hashedPassword });
-    res.status(201).json({
-        message: 'User created',
-        user,
-    });
+  const user = await User.create({ ...validated, password: hashedPassword });
+  res.status(201).json({
+    message: 'User created',
+    user,
+  });
 });
 
 /**
@@ -34,34 +32,33 @@ const register = asyncHanlder(async (req, res) => {
  * @returns {object} user
  */
 const login = asyncHanlder(async (req, res) => {
-    const { error: validationError, value: validated } =
-        loginValidation.validate(req.body, { abortEarly: false });
+  const { error: validationError, value: validated } = loginValidation.validate(
+    req.body,
+    { abortEarly: false }
+  );
 
-    if (validationError)
-        return res.json(parseValidationErrors(validationError.details));
+  if (validationError)
+    return res.json(parseValidationErrors(validationError.details));
 
-    const user = await User.findOne({ email: validated.email });
+  const user = await User.findOne({ email: validated.email });
 
-    const passwordMatch = await bcrypt.compare(
-        validated.password,
-        user.password
-    );
+  const passwordMatch = await bcrypt.compare(validated.password, user.password);
 
-    if (!passwordMatch) {
-        res.status(401).json({
-            status: 'unauthenticated',
-            message: 'Incorrect credentials',
-        });
-    }
-
-    res.json({
-        message: 'User found',
-        user,
-        token: generateToken(user._id),
+  if (!passwordMatch) {
+    res.status(401).json({
+      status: 'unauthenticated',
+      message: 'Incorrect credentials',
     });
+  }
+
+  res.json({
+    message: 'User found',
+    user,
+    token: generateToken(user._id),
+  });
 });
 
 module.exports = {
-    register,
-    login,
+  register,
+  login,
 };
