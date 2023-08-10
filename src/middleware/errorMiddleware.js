91606607961d1fc739594a1f7a445b5e602a9cd7
Sukkoth <suukootj@gmail.com>
2023-08-10 10:41:42 +0300

    added variations

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,10 +1,12 @@
-const errorMiddleware = (err, req, res, next) => {
-    if (err) {
-        return res.json({
-            message: err.message,
-            stack: err.stack,
-        });
-    }
+const errorMiddleware = (err, req, res) => {
+  const statusCode = res.statusCode != 200 ? res.statusCode : 500;
+
+  if (err) {
+    return res.status(statusCode).json({
+      message: err.message,
+      stack: err.stack,
+    });
+  }
 };
 
 module.exports = errorMiddleware;
