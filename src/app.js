1e8f3fa7045d0fb5e0e78f7781cdd463d0447fe5
Sukkoth <Suukootj@gmail.com>
2023-09-06 20:15:10 +0300

    homepage and 404 handler

--- src/app.js
+++ src/app.js
@@ -27,11 +27,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) =>
+  res.json({ message: 'Welcome to Eshop API Service' })
+);
 app.use('/api/products', require('./routes/Product/productRoutes'));
 app.use('/api/carts', require('./routes/Cart/cartRoutes'));
 app.use('/api/wishList', require('./routes/WishList/wishListRoutes'));
 app.use('/api/categories', require('./routes/categoryRoutes'));
 app.use('/api/users', require('./routes/User/userRoutes'));
+app.use('*', (req, res) =>
+  res.status(404).json({ message: 'Page not found', code: '404' })
+);
 app.use(errorMiddleware);
 
 module.exports = app;
