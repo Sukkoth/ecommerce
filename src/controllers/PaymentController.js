6784b8bdb99327275b85b4dab9a8193603c642f9
Sukkoth <suukootj@gmail.com>
2023-10-02 11:40:01 +0300

    payment done

--- src/controllers/PaymentController.js
+++ src/controllers/PaymentController.js
@@ -1,11 +1,11 @@
 const asyncHandler = require('express-async-handler');
 const Order = require('../models/Order');
+const Cart = require('../models/Cart');
 const stripe = require('stripe')(
   'sk_test_51KUHzQB3tJUunZUS9z4fDi5qeVEvesG3lyPB2RSWBZ4YWPH5MIevE43UjAuH3EVKyuwnVUL36w5DvRXgFSLnkRhY0051qSBCSN'
 );
 
 const env = require('../../config/env');
-console.log(env);
 
 const makePayment = asyncHandler(async (req, res) => {
   const orderId = req.query.orderId;
@@ -27,13 +27,37 @@ const makePayment = asyncHandler(async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items,
     mode: 'payment',
-    success_url: env.payment_success_redirect,
-    cancel_url: env.payment_failed_redirect,
+    metadata: {
+      orderId: orderId,
+    },
+    success_url:
+      env.app_url + '/payment-success?session_id={CHECKOUT_SESSION_ID}',
+    cancel_url: env.app_url + '/payment-failed',
   });
 
   res.redirect(303, session.url);
 });
 
+const paymentSuccess = asyncHandler(async (req, res) => {
+  const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+  const orderId = session.metadata.orderId;
+  const order = await Order.findByIdAndUpdate(
+    orderId,
+    { status: 'pending' },
+    { new: true }
+  );
+
+  const cart = await Cart.findOneAndUpdate({ user: order.user }, { items: [] });
+
+  res.redirect(302, env.front_end_url + '/orders');
+});
+
+const paymentFailed = asyncHandler(async (req, res) => {
+  return res.redirect(302, env.front_end_url + '/orders');
+});
+
 module.exports = {
   makePayment,
+  paymentFailed,
+  paymentSuccess,
 };
--- src/routes/Payment/paymentRoute.js
+++ src/routes/Payment/paymentRoute.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const paymentController = require('../../controllers/PaymentController');
 
 router.get('/create-checkout-session', paymentController.makePayment);
+router.get('/payment-success', paymentController.paymentSuccess);
+router.get('/payment-failed', paymentController.paymentFailed);
 
 module.exports = router;
